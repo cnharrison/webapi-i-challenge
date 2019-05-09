7473174300df37e0bd4e8c4e5a4c90c8bc68c27f
Christopher Harrison <cnharrison@gmail.com>
2019-05-08 20:10:26 -0400

    /api/users

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require("express");
+const server = express();
+
+const db = require("./data/db.js");
+
+server.listen(9090, () => {
+  console.log("server listening on port 9090 😂💯👌");
+});
+
+server.get("/", (req, res) => {
+  res.send("<h1>🙈🙉🙊🐵my server🐵🙊🙉🙈</h1>");
+});
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.send(users);
+    })
+    .catch(err => console.log(err));
+});
