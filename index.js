3dd8ee903bf3d2f3d63b053751943d4865aaa238
Christopher Harrison <cnharrison@gmail.com>
2019-05-08 21:31:11 -0400

    post, but i can't get findById() to work to give back newly added object

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // implement your API here
 const express = require("express");
 const server = express();
+server.use(express.json());
 
 const db = require("./data/db.js");
 
@@ -15,7 +16,20 @@ server.get("/", (req, res) => {
 server.get("/api/users", (req, res) => {
   db.find()
     .then(users => {
-      res.json(users);
+      res.json(users); 
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+server.post("/api/users", (req, res) => { 
+    const newUser = req.body;
+    if (!req.body.name || !req.body.bio) { 
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    }
+    db.insert(newUser)
+    .then(addedUser => { 
+        // res.status(201).json(db.findById(1));
+        res.status(201).json(addedUser);
     })
     .catch(err => res.status(500).send(err));
 });
